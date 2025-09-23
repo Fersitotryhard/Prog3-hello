11e5d9dc144380e230b7a76a09b5f353273aaab8
Fersitotryhard <147527507+erikdavid24@users.noreply.github.com>
2025-09-23 11:30:06 -0700

    NUEVA ACTUXD

---
+++ resources/script.js
@@ -0,0 +1,14 @@
+//evento que se ejecuta cuando el DOM ha sido completamente cargado
+//DOM: Document Object Model
+document.addEventListener('DOMContentLoaded', function() {
+    
+    //declaramos una variable que represente al boton
+    var btnOK = document.getElementById('miBoton');
+    //agregamos un evento click al boton
+    btnOK.addEventListener('click', function() {
+        //declaramos una variable que represente al input
+        var nombreGato = document.getElementById('input-name');
+        //mostrtamos una alerta con el valor del input
+        alert('El nombre de tu gato es: ' + nombreGato.value);
+    });
+});
